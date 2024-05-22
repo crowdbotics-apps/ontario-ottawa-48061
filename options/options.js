bcb396f95e20f24632a7431768540464ba54e562
Crowdbotics <team@crowdbotics.com>
2024-05-22 16:01:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ontario-ottawa-48061",
+  url: "https://ontario-ottawa-48061.botics.co",
+  api: "https://ontario-ottawa-48061.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
